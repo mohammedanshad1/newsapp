a89e56bda66e4dabc229ce7f334aeb2f82032d7a
Anshad <anshadk556@gmail.com>
2024-07-26 10:38:51 +0530

    added modal bottom sheet to show details of news

--- lib/view/news_view.dart
+++ lib/view/news_view.dart
@@ -40,44 +40,84 @@ class Homes extends StatelessWidget {
             itemCount: newsViewModel.news!.articles.length,
             itemBuilder: (context, index) {
               final article = newsViewModel.news!.articles[index];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network(
-                        article.urlToImage ??
-                            'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
-                        fit: BoxFit.fitHeight,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Image.network(
-                            'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
-                            fit: BoxFit.cover,
-                          );
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          article.title,
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              article.title,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              "Published at: ${article.publishedAt}",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              article.description ?? 'No description available',
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text(
-                          "Source: ${article.source.name}",
-                          style:
-                              const TextStyle(fontSize: 14, color: Colors.grey),
+                      );
+                    },
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          article.urlToImage ??
+                              'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
+                          fit: BoxFit.fitHeight,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Image.network(
+                              'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
+                              fit: BoxFit.cover,
+                            );
+                          },
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            article.title,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            "Source: ${article.source.name}",
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.grey),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
