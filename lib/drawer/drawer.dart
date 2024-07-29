d211a5df77ff56f19eff56ba001d90b265d24725
Anshad <anshadk556@gmail.com>
2024-07-29 16:25:56 +0530

    signing signup issues solved

--- lib/drawer/drawer.dart
+++ lib/drawer/drawer.dart
@@ -4,96 +4,138 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Drawericon extends StatelessWidget {
-  const Drawericon({
-    super.key,
-  });
+class Drawericon extends StatefulWidget {
+  const Drawericon({super.key});
+
+  @override
+  _DrawericonState createState() => _DrawericonState();
+}
+
+class _DrawericonState extends State<Drawericon> {
+  String? userEmail;
+  String? userProfilePic;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userEmail = prefs.getString('userEmail');
+      userProfilePic = prefs.getString('userProfilePic');
+    });
+  }
+
   Future<void> logout(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', false);
+    await prefs.remove('userEmail');
+    await prefs.remove('userProfilePic');
     await FirebaseAuth.instance.signOut();
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => Login()));
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => Login()),
+      (route) => false,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [
-        DrawerHeader(
-          decoration: BoxDecoration(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: HexColor("303050"),
+              border: const Border(
+                bottom: BorderSide(
+                  color: Colors.grey,
+                  width: 0.0,
+                ),
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (userProfilePic != null && userProfilePic!.isNotEmpty)
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(userProfilePic!),
+                      radius: 40,
+                    )
+                  else
+                    const Center(
+                      child: Icon(
+                        Icons.person,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                  const SizedBox(height: 10),
+                  if (userEmail != null)
+                    Text(
+                      userEmail!,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Sora",
+                      ),
+                    )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 20),
+            child: ListTile(
+              title: const Text(
+                "H O M E",
+                style: TextStyle(fontFamily: "Sora"),
+              ),
+              leading: const Icon(Icons.home),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: ListTile(
+              title: const Text(
+                "S E T T I N G S",
+                style: TextStyle(fontFamily: "Sora"),
+              ),
+              leading: const Icon(Icons.settings),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          const Spacer(),
+          Container(
             color: HexColor("303050"),
-            border: const Border(
-              bottom: BorderSide(
-                color: Colors.grey,
-                width: 0.0,
-              ),
-            ),
-          ),
-          child: const Center(
-            child: Icon(
-              Icons.person,
-              size: 50,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10, top: 20),
-          child: ListTile(
-            title: const Text(
-              "H O M E",
-              style: TextStyle(fontFamily: "Sora"),
-            ),
-            leading: const Icon(Icons.home),
-            onTap: () {
-              //pop the drawer
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: ListTile(
-            title: const Text(
-              "S E T T I N G S",
-              style: TextStyle(fontFamily: "Sora"),
-            ),
-            leading: const Icon(Icons.settings),
-            onTap: () {
-              //pop the drawer
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        const Spacer(),
-        Container(
-          color: HexColor("303050"),
-          child: ListTile(
-            leading: const Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-            title: const Text(
-              'Logout',
-              style: TextStyle(
+            child: ListTile(
+              leading: const Icon(
+                Icons.logout,
                 color: Colors.white,
-                fontFamily: "Sora",
               ),
+              title: const Text(
+                'Logout',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Sora",
+                ),
+              ),
+              onTap: () async {
+                await logout(context);
+              },
             ),
-            onTap: () async {
-              // Handle logout action
-              logout(context);
-              Navigator.pop(context); // Close the drawer
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          Login())); // Navigate to login screen
-            },
-          ),
-        )
-      ]),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/emailverifaction/firehelper.dart
+++ lib/emailverifaction/firehelper.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firestoreproject/emailverifaction/snackbar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../view/news_view.dart';
 
 class firebase {
@@ -94,43 +95,43 @@ class firebase {
   }
 
   /// Google SIGN-IN
+
+  /// Google SIGN-IN
   Future<void> signInWithGoogle(BuildContext context) async {
     try {
-      // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-      // If the user cancels the sign-in flow, googleUser will be null
       if (googleUser == null) {
         showSnackBar(context, "Google sign-in was cancelled.");
         return;
+      } else {
+        showSnackBar(context, "Logging Successfull");
       }
-
-      // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
           await googleUser.authentication;
 
-      // Create a new credential
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
 
-      // Once signed in, return the UserCredential
       UserCredential userCredential =
           await FirebaseAuth.instance.signInWithCredential(credential);
 
-      // Store user email in Firestore
       await _firestore.collection('users').doc(userCredential.user!.uid).set({
         'email': googleUser.email,
       });
 
-      // Navigate to the home screen if the sign-in is successful
+      // Store email and profile picture URL in SharedPreferences
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('userEmail', googleUser.email);
+      await prefs.setString('userProfilePic', googleUser.photoUrl ?? '');
+
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => Homes()),
       );
     } catch (e) {
-      // Handle error
       showSnackBar(context, "Failed to sign in with Google: $e");
     }
   }
